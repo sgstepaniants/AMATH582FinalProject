c28b3fc478ec4b28903704651afee052e032ee62
George Stepaniants <George_Stepaniants@D-173-250-235-232.dhcp4.washington.edu>
2017-02-28 18:58:34 -0800

    Got 4 months with hours

--- UrlContent.java
+++ UrlContent.java
@@ -10,7 +10,7 @@ public class UrlContent{
         int year = 2015;
         
         // make 12 folders for each month
-        for (int month = 1; month <= 12; month++) {
+        for (int month = 1; month <= 4; month++) {
             File directory = new File(String.format("%02d", month));
             if (!directory.exists()){
                 directory.mkdir();
@@ -20,15 +20,17 @@ public class UrlContent{
             int daysInMonth = mycal.getActualMaximum(Calendar.DAY_OF_MONTH);
             
             for (int day = 1; day <= daysInMonth; day++) {
-                try {
-                    Date date = new Date(0, day, month, year);
-                    URL url = new URL("https://www.ncdc.noaa.gov/gibbs/image/GRD-1/WV/" + date.toString());
-                    System.out.println(url);
-                    image = ImageIO.read(url);
-        
-                    ImageIO.write(image, "jpg", new File(String.format("%02d", month) + "/" + date.toString() + ".jpg"));
-                } catch (IOException e) {
-                	e.printStackTrace();
+                for (int hour = 0; hour <= 24; hour += 3) {
+                    try {
+                        Date date = new Date(hour, day, month, year);
+                        URL url = new URL("https://www.ncdc.noaa.gov/gibbs/image/MSG-3/WV/" + date.toString());
+                        System.out.println(url);
+                        image = ImageIO.read(url);
+            
+                        ImageIO.write(image, "jpg", new File(String.format("%02d", month) + "/" + date.toString() + ".jpg"));
+                    } catch (IOException e) {
+                    	e.printStackTrace();
+                    }
                 }
             }
         }
